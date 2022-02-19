b5ce84c7f0bcb545d1979727a53afac9c0ce77fb
stevy <stevyralambomanana@gmail.com>
2022-02-19 22:22:24 +0100

    add route

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,17 @@
 <?php
 
-use App\Http\Controllers\admin\AdminAuthController;
-use App\Http\Controllers\admin\HomeController;
-use App\Http\Controllers\api\AppController;
-use App\Http\Controllers\api\authController;
-use App\Http\Controllers\api\ChatController;
-use App\Http\Controllers\api\FriendsController;
-use App\Http\Controllers\api\GroupController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\ChatController;
+use App\Http\Controllers\api\UserController;
+use App\Http\Controllers\api\GroupController;
+use App\Http\Controllers\admin\HomeController;
+use App\Http\Controllers\api\FriendsController;
+use App\Http\Controllers\api\InvitationController;
+use App\Http\Controllers\admin\AdminAuthController;
+use App\Http\Controllers\api\CommentaireController;
+use App\Http\Controllers\api\PublicationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,8 +30,8 @@ use Illuminate\Support\Facades\Route;
 
 // test de git 2
 
-Route::post('login',[authController::class,'login']);
-Route::post('register',[authController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
+Route::post('register',[AuthController::class,'register']);
 
 Route::post('/admin/login',[AdminAuthController::class,'login']);
 Route::post('/admin/register',[AdminAuthController::class,'register']);
@@ -40,40 +43,54 @@ Route::any('/unauth',function(){
 })->name('unauthentified');
 
 Route::middleware('auth:user')->group(function() {
-    Route::get('/user',[AppController::class, 'profile']);
+    Route::get('/user',[UserController::class, 'myProfile']);
+    Route::get('/otherProfil',[UserController::class, 'otherProfil']);
+    Route::post('/user/complete-profil',[UserController::class, 'completeProfile']);
 
 
     //chat
-    Route::get('/chat/allConversation',[ChatController::class, 'allConversation']);
-    Route::get('/chat/allDiscussion/{conversation}',[ChatController::class, 'allDiscussion']);
-    Route::get('/chat/verifiConvExist/{user}',[ChatController::class, 'verification']);
-    Route::post('/chat/sendMessage/',[ChatController::class, 'sendMessage']);
-    Route::post('/chat/create-conversation',[ChatController::class, 'createConversation']);
+    Route::get('/chat/allConversation',[ChatController::class, 'allConversation']); //liste tout mes conversation
+    Route::get('/chat/allDiscussion/{id_conversation?}',[ChatController::class, 'allDiscussion']); //liste tout les message dans une conversation
+    Route::post('/chat/sendMessage',[ChatController::class, 'sendMessage']); //envoyer message
 
 
-    //publication
+
+    //publication statut
+    Route::post('/publier/create',[PublicationController::class, 'publierStatut']); //publiez status
+    Route::post('/publier/delete/{id_publication}',[PublicationController::class, 'supprimerStatut']); //supprimer statut
+    Route::post('/publier/update/{id_publication}',[PublicationController::class, 'modifier']); // modifier statut
+    Route::get('/publier/view/{id_publication}',[PublicationController::class, 'viewStatut']); //voir status
+
 
 
     //groupe
-    Route::post('/group/create',[GroupController::class, 'creategroupe']);
-    Route::get('/group/membres/{group_id}',[GroupController::class, 'getMembreGroup']);
+    Route::post('/group/create',[GroupController::class, 'creategroupe']); //creer une groupe
+    Route::get('/group/membres/{group_id}',[GroupController::class, 'getMembreGroup']); //tout les user membres groupe
+    Route::get('/group',[GroupController::class, 'groupe']); //affiche mes groupe
 
 
 
     //commentaire
-
-
-
+    Route::post('/commentaire/create',[CommentaireController::class, 'createCommentaire']);
+    Route::post('/commentaire/delete',[CommentaireController::class, 'deleteCommentaire']);
+    Route::post('/commentaire/modifier',[CommentaireController::class, 'modifierCommentaire']);
 
     //reactions
 
 
 
 
-    //invitaiton
-    Route::post('/invitation/invit-amis',[FriendsController::class, 'inviteUser']);
-    Route::post('/invitation/accept',[FriendsController::class, 'accepteAmis']);
+    //invitation
+    Route::post('/invitation/invit-amis',[InvitationController::class, 'inviteUserEnAmis']); //inviter une user en amis
+    Route::post('/invitation/accept',[InvitationController::class, 'accepteEnAmis']); //accetpe une invitation en amis
+    Route::get('/invitation/all-invitation-no',[InvitationController::class, 'getAllMyInvitation']); //tout les invitations que jai envoyer mais pas encore accepter
+    Route::get('/invitation/all-invitation-yes',[InvitationController::class, 'getAllMyInvitation']); //tout les invitation que je recois mes pas encore accepter
 
+
+
+    // friends
+    Route::get('/friends/all-amis',[FriendsController::class, 'getAllFriends']); //tout mes amis
+    Route::post('/friends/retirer/{user_id}',[FriendsController::class, 'retirer']); //retirer amis
 });
 
 Route::middleware('auth:admin')->group(function() {
