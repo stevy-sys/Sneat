00a7b30b21797f8c678a1aa4aa99666bbe0a613a
stevy <stevyralambomanana@gmail.com>
2022-02-19 22:14:10 +0100

    creation media file

---
+++ database/migrations/2022_02_19_131734_create_media_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->string('file');
+            $table->string('type');
+            $table->integer('mediable_id');
+            $table->string('mediable_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
