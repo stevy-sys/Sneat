5aa8b1fb8fc93fe078ad16afd775712a2b485ebd
stevy <stevy@localhost.localdomain>
2022-02-19 07:16:22 +0100

    test

--- routes/api.php
+++ routes/api.php
@@ -23,8 +23,9 @@ use Illuminate\Support\Facades\Route;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
-// });
+// }); 
 
+// test de git
 Route::post('login',[authController::class,'login']);
 Route::post('register',[authController::class,'register']);
 
