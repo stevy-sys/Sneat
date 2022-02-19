dcfb9a5442967b6d4578d5674d1e9c33b082c874
stevy <stevyralambomanana@gmail.com>
2022-02-19 22:19:01 +0100

    crud pubplication

---
+++ app/Http/Controllers/api/PublicationController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Publication;
+use App\Models\User;
+use Illuminate\Http\Client\Request as ClientRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PublicationController extends Controller
+{
+    protected $auth ;
+    public function __construct() {
+
+        $this->auth = User::find(Auth::id());
+    }
+
+    public function publierStatut (Request $request)
+    {
+        $publication = $this->auth->publicationStatus()->create([
+            'user_id' => Auth::id(),
+            'description' => $request->description
+        ]);
+        return response()->json([
+            'data' => $publication
+        ],201);
+    }
+
+    public function supprimerStatut($id_publication)
+    {
+        Publication::find($id_publication)->delete();
+        return response()->json([
+            'message' => 'publication delete'
+        ],201);
+    }
+
+    public function modifier($id_publication,Request $request)
+    {
+        $publication = Publication::find($id_publication) ;
+        $publication->update([
+            'description' => $request->description
+        ]);
+        return response()->json([
+            'message' => 'publication delete'
+        ],201);
+    }
+
+    public function viewStatut($id_publication)
+    {
+        $publication = Publication::find($id_publication);
+        return response()->json([
+            'message' => $publication
+        ],201);
+    }
+}
