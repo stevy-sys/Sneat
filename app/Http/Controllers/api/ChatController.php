517fa918c735663983958b97a2602cfa857b3df4
stevy <stevyralambomanana@gmail.com>
2022-02-19 22:15:21 +0100

    optimisation conversation verif et create

--- app/Http/Controllers/api/ChatController.php
+++ app/Http/Controllers/api/ChatController.php
@@ -7,27 +7,66 @@ use App\Models\Conversation;
 use App\Models\Message;
 use App\Models\MembreConversation;
 use App\Models\User;
+use Facade\FlareClient\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller
 {
 
-    public function createConversation(Request $request)
+    protected $auth ;
+    public function __construct() {
+
+        $this->auth = User::find(Auth::id());
+    }
+
+    private function createConversation($request)
     {
         try {
+            
             $conversation = Conversation::create([
-                'name' => $request->name,
+                'name' => $request->name ? $request->name : null ,
                 'type' => $request->type
             ]);
+                
+            return $conversation ;
+        } catch (\Throwable $th) {
+            dd($th->getMessage());
+        }
+    }
 
-            return response()->json([
-                'conversation' => $conversation
-            ],201);
+    private function createMembre($conversation,$id_user)
+    {
+        $conversation = Conversation::find($conversation);
+        $conversation->membres()->create([
+            'user_id' => Auth::id()
+        ]);
+        $conversation->membres()->create([
+            'user_id' => $id_user
+        ]);
+    }
+
+    private function verification($user_id)
+    {
+        try {
+            $user = User::find($user_id);
+            $idConversation = null;
+            $lists = MembreConversation::with(['conversation.membres' => function($q) use ($user){
+                $q->where('user_id',$user->id);
+            }])->where('user_id',Auth::id())->get()->pluck('conversation.membres');
+
+            foreach ($lists as $list) {
+                if (count($list) > 0) {
+                    $idConversation = $list[0]->conversation_id ;
+                }
+            }
+            return $idConversation ;       
         } catch (\Throwable $th) {
             return response()->json(['error' => $th->getMessage()]);
         }
     }
+
+
 
     public function AllConversation()
     {
@@ -43,45 +82,52 @@ class ChatController extends Controller
         }
     }
 
-    public function allDiscussion(Conversation $conversation)
+
+    public function allDiscussion($id_conversation = null, Request $request)
     {
         try {
-            $discussion = Message::with('user')->where('conversation_id',$conversation->id)->get();
+            
+            if (!$id_conversation) {
+                $id_conversation = $this->verification($request->user_id);
+                if (!$id_conversation) {
+                    return response()->json([
+                        'conversation' => null
+                    ],201);
+                }
+            }
+
+            $discussion = Message::with('user')->where('conversation_id',$id_conversation)->get();
             return response()->json([
                 'conversation' => $discussion
             ],201);
+
         } catch (\Throwable $th) {
             return response()->json(['error' => $th->getMessage()]);
         }
         
     }
 
-    public function verification(User $user)
-    {
-        try {
-            $idConversation = null;
-            $lists = MembreConversation::with(['conversation.membres' => function($q) use ($user){
-                $q->where('user_id',$user->id);
-            }])->where('user_id',Auth::id())->get()->pluck('conversation.membres');
-
-            foreach ($lists as $list) {
-                if (count($list) > 0) {
-                    $idConversation = $list[0]->conversation_id ;
-                }
-            }
-            return ['conversation_id'=>$idConversation];       
-        } catch (\Throwable $th) {
-            return response()->json(['error' => $th->getMessage()]);
-        }
-    }
+   
 
     public function sendMessage(Request $request)
     {
         try {
-            $message = Auth::user()->Messages()->create([
-                'conversation_id' => $request->conversation_id,
-                'message' => $request->messages
-            ]);
+
+            $id_conversation = $this->verification($request->id_user);
+            if (!$id_conversation) {
+                $conversation = $this->createConversation($request);
+                $this->createMembre($conversation->id,$request->id_user);
+                $message =  $this->auth->Messages()->create([
+                    'conversation_id' => $conversation->id,
+                    'message' => $request->messages
+                ]);
+            }else{
+                $message =  $this->auth->Messages()->create([
+                    'conversation_id' => $id_conversation,
+                    'message' => $request->messages
+                ]);
+            }
+
             return response()->json([
                 'conversation' => $message
             ],201);
