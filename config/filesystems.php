a51a05819838fdd6a7fff0e537aad0da7da46d1f
stevy <stevyralambomanana@gmail.com>
2022-02-26 16:01:01 +0100

    config path pub profil

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,6 +42,19 @@ return [
             'visibility' => 'public',
         ],
 
+        'profil' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/profil'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+        'publication' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/publication'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
