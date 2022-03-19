a59a627f4cc9aaf8f46c963c44d95142fbc420f8
stevy <stevyralambomanana@gmail.com>
2022-03-19 06:56:53 +0100

    config websocket

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\WebsocketDemo;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    broadcast( new WebsocketDemo('2'))->toOthers();
     return view('welcome');
 });
