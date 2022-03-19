ed30949fbc80fcc30a972f194f1c4b297c58f805
stevy <stevyralambomanana@gmail.com>
2022-03-19 20:59:25 +0100

    delete test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\NotifConversation;
 use App\Events\WebsocketDemo;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    broadcast( new WebsocketDemo('2'))->toOthers();
+    // broadcast( new NotifConversation('1'))->toOthers();
     return view('welcome');
 });
