acea839e58edc462140b28d6d6843d50b2c95dbe
stevy <stevyralambomanana@gmail.com>
2023-09-30 16:26:58 +0200

    actu new membre and old member

--- app/Http/Controllers/api/ActualiteController.php
+++ app/Http/Controllers/api/ActualiteController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers\api;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Models\Actualites;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\MembreGroup;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 /**
  * @OA\Info(
@@ -54,6 +56,31 @@ class ActualiteController extends Controller
     public function getActualite()
     {
         $amis_id = Auth::user()->friends->pluck('id');
+        if ((count($amis_id) == 0)) {
+            $membres = MembreGroup::where('user_id',Auth::id())->first();
+            if (!isset($membres)) {
+                $actualite = Actualites::whereHas(
+                    'actualable',
+                    function ($query) use ($amis_id) {
+                        $query->WhereHasMorph('publicable', ['App\Models\User'], function ($q) use ($amis_id) {
+                            // $q->whereIn('id', $amis_id);
+                        })->orWhereHasMorph('publicable', ['App\Models\Shares'], function ($query) use ($amis_id) {
+                            // $query->whereIn('user_id', $amis_id);
+                        });
+                    },
+                )->with([
+                        'actualable.user',
+                        'actualable.publicable.sharable.user',
+                        'actualable.publicable.sharable.media',
+                        'actualable.publicable.sharable.commentaires.user',
+                        'actualable.media',
+                        'actualable.commentaires.user'
+                    ])->inRandomOrder()->orderBy('created_at','desc')->paginate(5);
+                return response()->json([
+                    'data' => $actualite
+                ], 201);
+            }
+        }
         $actualite = Actualites::whereHas(
             'actualable',
             function ($query) use ($amis_id) {
@@ -65,6 +92,71 @@ class ActualiteController extends Controller
                     });
                 })->orWhereHasMorph('publicable', ['App\Models\Shares'], function ($query) use ($amis_id) {
                     $query->whereIn('user_id', $amis_id);
+                });
+            },
+        )->with([
+                'actualable.user',
+                'actualable.publicable.sharable.user',
+                'actualable.publicable.sharable.media',
+                'actualable.publicable.sharable.commentaires.user',
+                'actualable.media',
+                'actualable.commentaires.user'
+            ])->inRandomOrder()->orderBy('created_at','desc')->paginate(5);
+            
+        return response()->json([
+            'data' => $actualite
+        ], 201);
+    }
+
+
+    /**
+     * @OA\Get(
+     *      path="/api/actualite/{user}",
+     *      operationId="getActualityProfile",
+     *      tags={"Actualite"},
+     *      summary="Obtenir la liste des actualités d'un profile",
+     *      description="Retourne la liste des publication ou partage qu'un profile a deja fait",
+     *      @OA\Parameter(
+     *          name="user",
+     *          in="path",
+     *          required=true,
+     *          description="ID user",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="page",
+     *          in="query",
+     *          required=false,
+     *          description="pagination",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *        @OA\Response(
+     *          response=200,
+     *          description="Liste des actualités",
+     *          @OA\JsonContent(
+     *              type="object",
+     *          )
+     *      )
+     * )
+     */
+    public function getActualityProfile(User $user) {
+        $actualite = Actualites::whereHas(
+            'actualable',
+            function ($query) use ($user) {
+                $query->WhereHasMorph('publicable', ['App\Models\User'], function ($q) use ($user) {
+                    $q->where('id', $user->id);
+                })
+                // ->orWhereHasMorph('publicable', ['App\Models\Group'], function ($query) {
+                //     $query->whereHas('membresGroupe', function ($query) {
+                //         $query->where('user_id', Auth::id());
+                //     });
+                // })
+                ->orWhereHasMorph('publicable', ['App\Models\Shares'], function ($query) use ($user) {
+                    $query->where('user_id', $user->id);
                 });
             },
         )->with([
